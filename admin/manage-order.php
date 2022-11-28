82784d7c4ca80bb0c4bb26f48613980e8024f465
abhi587 <abhishek.pawan14@gmail.com>
2022-11-28 12:18:38 +0530

    done

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -4,57 +4,111 @@
     <div class="wrapper">
     <h1>Manage Order</h1>
 
-    
 
             <br /> <br /> <br />
 
+            <?php
+                if(isset($_SESSION['update']))
+                {
+                    echo $_SESSION['update'];
+                    unset($_SESSION['update']);
+                }
+            ?>
+            <br><br>
 
             <table class="tbl-full">
                 <tr>
 
                     <th>SNo.</th>
-                    <th>full Name</th>
-                    <th>User Name</th>
-                    <th>Actions</th>
+                    <th>Food</th>
+                    <th>Price</th>
+                    <th>Qty.</th>
+                    <th>Total</th>
+                    <th>Order date</th>
+                    <th>Status</th>
+                    <th>Customer Name</th>
+                    <th>contact</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Action</th>
                 </tr>
 
-                <tr>
-                    <td>1.</td>
-                    <td>Abhisek prasad</td>
-                    <td>abhishekprasad</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
+                <?php
+                    //GEt all the orders from database
+                    $sql = "SELECT * FROM tbl_order ORDER BY id DESC";  //Display the latest order at first 
+                    //Execute Query
+                    $res = mysqli_query($conn, $sql);
+                    //count the rows
+                    $count = mysqli_num_rows($res);
 
-                    </td>
+                    $sn = 1; //create a serial number and set its initial value as 1
 
-                </tr>
+                    if($count>0)
+                    {
+                        //order available
+                        while($row=mysqli_fetch_assoc($res))
+                        {
+                            //Get all the order Details
+                            $id = $row['id'];
+                            $food = $row['food'];
+                            $price = $row['price'];
+                            $qty = $row['qty'];
+                            $total = $row['total'];
+                            $order_date = $row['order_date'];
+                            $status = $row['status'];
+                            $customer_name = $row['customer_name'];
+                            $customer_contact = $row['customer_contact'];
+                            $customer_email = $row['customer_email'];
+                            $customer_address = $row['customer_address'];
 
-                <tr>
-                    <td>2.</td>
-                    <td>Abhisek prasad</td>
-                    <td>abhishekprasad</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
+                            ?>
+                                <tr>
+                                    <td><?php echo $sn++; ?>. </td>
+                                    <td><?php echo $food; ?></td>
+                                    <td><?php echo $price; ?></td>
+                                    <td><?php echo $qty; ?></td>
+                                    <td><?php echo $total; ?></td>
+                                    <td><?php echo $order_date; ?></td>
 
+                                    <td>
+                                        <?php   
+                                            //ordered, on delivery ,delivered, canceled
+                                            if($status=="Ordered")
+                                            {
+                                                echo "<label>$status</label>";
+                                            }
+                                            elseif($status=="On Delivery")
+                                            {
+                                                echo "<label style='color:orange'>$status</label>";
+                                            }
+                                            elseif($status=="Delivered")
+                                            {
+                                                echo "<label style='color:green'>$status</label>";
+                                            }
+                                            elseif($status=="Cancelled")
+                                            {
+                                                echo "<label style='color:red'>$status</label>";
+                                            }
+                                        ?>
+                                    </td>
 
-                    </td>
-
-                </tr>
-
-                <tr>
-                    <td>3.</td>
-                    <td>Abhisek prasad</td>
-                    <td>abhishekprasad</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-
-
-                    </td>
-
-                </tr>
+                                    <td><?php echo $customer_name; ?></td>
+                                    <td><?php echo $customer_contact; ?></td>
+                                    <td><?php echo $customer_email; ?></td>
+                                    <td><?php echo $customer_address; ?></td>
+                                    <td>
+                                        <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>" class="btn-secondary">Update Order</a>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+                    }
+                    else
+                    {
+                        //order not available
+                        echo "<tr><td colspan='12' class='error'>Order not Available</td></tr>";
+                    }
+                ?>
                 
 
             </table>
--- category-foods.php
+++ category-foods.php
@@ -6,11 +6,22 @@
     {
         //category id is set and get the id 
         $category_id=$_GET['category_id'];
+        //Get the category title Based on category Id
+        $sql = "SELECT title FROM tbl_category WHERE id = $category_id";
+
+        //Execute the query
+        $res = mysqli_query($conn, $sql);
+
+        //Get the value from database
+        $row = mysqli_fetch_assoc($res);
+        //get the title
+        $category_title = $row['title'];
     }
     else
     {
         //category not passed
         //redirect to homepage
+        header('location:'.SITEURL);
     }
 ?>
 
@@ -18,7 +29,7 @@
     <section class="food-search text-center">
         <div class="container">
             
-            <h2>Foods on <a href="#" class="text-white">"Category"</a></h2>
+            <h2>Foods on <a href="#" class="text-white">"<?php echo $category_title ?>"</a></h2>
 
         </div>
     </section>
@@ -31,108 +42,70 @@
         <div class="container">
             <h2 class="text-center">Food Menu</h2>
 
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
+            <?php 
+            
+                //create sql query to get foods based on selected category
+                $sql2 = "SELECT * FROM tbl_food WHERE category_id= $category_id";
 
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
+                //Execute the query 
+                $res2 = mysqli_query($conn, $sql2);
 
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
+                //count the rows
+                $count2 = mysqli_num_rows($res2);
 
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
+                //check whether food is available or not
+                if($count2>0)
+                {
+                    //food is available
+                    while($row2 = mysqli_fetch_assoc($res2))
+                    {
+                        $id = $row2['id'];
+                        $title = $row2['title'];
+                        $price = $row2['price'];
+                        $description = $row2['description'];
+                        $image_name = $row2['image_name'];
+                        ?>
 
-                <div class="food-menu-desc">
-                    <h4>Smoky Burger</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
+                            <div class="food-menu-box">
+                                <div class="food-menu-img">
+                                    <?php
+                                        if($image_name=="")
+                                        {
+                                            //Image not Available
+                                            echo "<div class='error'>Image not Available.</div>";
+                                        }
+                                        else
+                                        {
+                                            // Image Available
+                                            ?>
+                                                <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                                            <?php
+                                        }
+                                    ?>
+                                    
+                                </div>
 
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
+                                <div class="food-menu-desc">
+                                    <h4><?php echo $title ?></h4>
+                                    <p class="food-price">$<?php echo $price; ?></p>
+                                    <p class="food-detail">
+                                        <?php echo $description; ?>
+                                    </p>
+                                    <br>
+                                    <a href="<?php echo SITEURL; ?>order.php?food_id=<?php echo $id; ?>" class="btn btn-primary">Order Now</a>
+                                </div>
+                            </div>
 
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-burger.jpg" alt="Chicke Hawain Burger" class="img-responsive img-curve">
-                </div>
+                        <?php
+                    }
+                }
+                else
+                {
+                    //food not available
+                    echo "<div class = 'error'>Food Not Available</div>";
+                }
 
-                <div class="food-menu-desc">
-                    <h4>Nice Burger</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-momo.jpg" alt="Chicke Hawain Momo" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Chicken Steam Momo</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
+            ?>
 
             <div class="clearfix"></div>
 
